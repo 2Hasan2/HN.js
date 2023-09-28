007780fbc3e1c35b810e6b0d76921e7af215bf16
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2023-09-29 01:08:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,35 +37,47 @@ function logDOMTree(node, depth = 0, tree = []) {
     return tree;
 }
 // to show the dom tree
-function ShowTree(tree) {
-    tree.forEach(element => {
-        let div = document.createElement('div');
-        let depth = (element.split(' ').length - 1) / 3;
-        div.style.color = '#' + (depth * 1111111).toString(16);
-        div.innerText = element;
-        domTree.appendChild(div);
-        div.addEventListener('click', function () {
-            if (selectedEle) {
-                selectedEle.classList.remove('selected');
-            }
-            selectedEle = iframeDocument.getElementById(`${div.innerText.split('#')[1]}`);
-            selectedEle.classList.add('selected');
-            bindElementStyle(selectedEle)
-        });
+function showElement(element){
+    let div = document.createElement('div');
+    let depth = (element.split(' ').length - 1) / 3;
+    div.style.color = '#' + (depth * 1111111).toString(16);
+    div.innerText = element;
+    domTree.appendChild(div);
+    div.addEventListener('click', function () {
+        if (selectedEle) {
+            selectedEle.classList.remove('selected');
+        }
+        selectedEle = iframeDocument.getElementById(`${div.innerText.split('#')[1]}`);
+        selectedEle.classList.add('selected');
+        bindElementStyle(selectedEle)
     });
 }
 
+function ShowTree(tree) {
+    domTree.innerHTML ="";
+    tree.forEach(element => showElement(element));
+
+}
+
 // to add element
+const addElementButton = document.getElementById('addElementButton')
 function addElement(tagName, parent) {
     let element = document.createElement(tagName);
     element.innerText = 'new element';
     element.id = iframeBody.children.length + 1;
     parent.appendChild(element);
+    
 }
+addElement('div',iframeBody);
+
+addElementButton.onclick = e => {
 addElement('div', iframeBody);
-addElement('div', iframeBody);
-addElement('div', iframeBody);
-addElement('div', iframeBody);
+// const last = iframeBody.children.last;
+ShowTree(logDOMTree(iframeBody));
+
+
+}
+
 
 // to remove element
 function removeElement(element) {
@@ -83,18 +95,32 @@ function appandElement(element, parent) {
 }
 
 // must use after add elements to iframe
-ShowTree(logDOMTree(iframeBody))
+
 
 
 // bind the selected element styles to the input use jquery
 function bindElementStyle(element) {
-    let style = window.getComputedStyle(element);
-    console.log(style.width);
-    console.log(style.height);
-    console.log(style.left);
-    console.log(style.top);
-    $('#w').val(style.width.split('px')[0]);
-    $('#h').val(style.height.split('px')[0]);
-    $('#x').val(style.left.split('px')[0]);
-    $('#y').val(style.top.split('px')[0]);
+
+    let style = element.style;
+    
+
+    // console.log(style.width);
+    // console.log();
+    // console.log(style.left);
+    // console.log(style.top);
+
+    // const W = document.getElementById('w');
+    // W.value=style.width;
+    // console.log(style.width);
+
+    // W.value=style.width.split('px')[0];
+    // style.width= W.value;
+    // console.log("W",W.value);
+
+    // $('#w').val(style.width.split('px')[0]);
+    // $('#h').val(style.height.split('px')[0]);
+    // $('#x').val(style.left.split('px')[0]);
+    // $('#y').val(style.top.split('px')[0]);
 }
+
+
