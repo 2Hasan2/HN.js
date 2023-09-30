f6b901956f0268ef7ea6a6399eb8b7ce00c4d5d3
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2023-09-30 20:09:49 +0300

    changed cursor in figma folder

--- test/figma/script.js
+++ test/figma/script.js
@@ -1,17 +1,40 @@
 // to give the user the ability to resize and rotate the element like in Figma
-function makeResizableAndRotatable(element) {
+function makeResizableAndRotatable (element){
     let isResizing = false;
     let isRotating = false;
     let startX, startY, startWidth, startHeight, startAngle;
+
+    document.addEventListener('mouseover', (e) => {
+        // log mouse position
+        if (isCloseToCorner(e, element)) {
+            element.style.cursor ='-webkit-grab';
+            // element.style.cursor = "url('rotation-cursor.png'), auto";
+        } else if (isCloseTo(e, element,10)) {
+            element.style.cursor ='ew-resize'; 
+        }
+        else{
+            element.style.cursor='auto'
+        }
+    });
+
+    document.addEventListener('mouseout', (e) => {
+        // log mouse position
+        if (isCloseToCorner(e, element,20)) {
+            element.style.cursor ='auto';
+        } else if (isCloseTo(e, element,10)) {
+            element.style.cursor ='auto'; 
+        }
+    });
+    
+
     document.addEventListener('mousedown', (e) => {
         // log mouse position
-        if (isCloseToCorner(e, element, 20)) {
+        if (isCloseToCorner(e, element,20)) {
             isRotating = true;
             startX = e.clientX;
             startY = e.clientY;
             startAngle = getRotationAngle(element);
-        } else if (isCloseTo(e, element, 10)) {
-            console.log('resizing');
+        } else if (isCloseTo(e, element,10)) {
             isResizing = true;
             startX = e.clientX;
             startY = e.clientY;
@@ -19,6 +42,7 @@ function makeResizableAndRotatable(element) {
             startHeight = element.offsetHeight;
         }
     });
+    
 
     document.addEventListener('mousemove', (e) => {
         if (isResizing) {
@@ -35,28 +59,27 @@ function makeResizableAndRotatable(element) {
     document.addEventListener('mouseup', () => {
         isResizing = false;
         isRotating = false;
+
     });
-
-    // Helper function to calculate the angle between two points
-    function getAngle(x1, y1, x2, y2) {
-        const deltaX = x1 - x2;
-        const deltaY = y1 - y2;
-        const radians = Math.atan2(deltaY, deltaX);
-        return radians * (180 / Math.PI);
-    }
-
-    // Helper function to get the current rotation angle of an element
-    function getRotationAngle(element) {
-        const transform = window.getComputedStyle(element).getPropertyValue('transform');
-        const matrix = new DOMMatrix(transform);
-        return Math.atan2(matrix.b, matrix.a) * (180 / Math.PI);
-    }
 }
-
-// makeResizableAndRotatable(element);
-
+    // Helper function to calculate the angle between two points
+makeResizableAndRotatable(element);
 
 // helper functions
+
+function getAngle(x1, y1, x2, y2) {
+    const deltaX = x1 - x2;
+    const deltaY = y1 - y2;
+    const radians = Math.atan2(deltaY, deltaX);
+    return radians * (180 / Math.PI);
+}
+
+// Helper function to get the current rotation angle of an element
+function getRotationAngle(element) {
+    const transform = window.getComputedStyle(element).getPropertyValue('transform');
+    const matrix = new DOMMatrix(transform);
+    return Math.atan2(matrix.b, matrix.a) * (180 / Math.PI);
+}
 
 // check if mouse is close to any corner of an element
 function isCloseToCorner(e, element, R = element.offsetWidth / 6) {
@@ -74,6 +97,7 @@ function isCloseToCorner(e, element, R = element.offsetWidth / 6) {
     }
     return false;
 }
+
 // check if mouse is close to an element
 function isCloseTo(e, element, R = element.offsetWidth / 6) {
     const { offsetLeft, offsetWidth, offsetTop, offsetHeight } = element;
