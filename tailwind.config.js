d25929b61eba7c93569f17370c74c5613baa9e9d
2Hasan2 <h01554176846@gmail.com>
2024-02-08 23:16:39 +0200

    setup env

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{ts,tsx,html,css,scss,sass,less,stylus}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
